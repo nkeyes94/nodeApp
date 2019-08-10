fe8cd2929317edf5b20305fbe203e3f7a80095fb
Nathan Keyes <n.keyes1994@gmail.com>
2019-08-10 13:19:09 -0400

    Updating with DB schema

---
+++ database.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS natemart_db;
+CREATE DATABASE natemart_db;
+
+USE natemart_db;
+
+CREATE TABLE listings(
+	id INT NOT NULL AUTO_INCREMENT,
+    item_name VARCHAR(100) NOT NULL,
+    amount INT NOT NULL,
+    PRIMARY KEY(id)
+);
